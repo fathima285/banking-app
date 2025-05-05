9280caddbadad9de5738c13ba8986294d57d7d7a
Mohamed Jaroof Fathima Sajiy <UT010665@unicomtic.com>
2025-05-05 14:47:25 +0530

    mypro

---
+++ consoleapp.py
@@ -0,0 +1,16 @@
+Account_file="accounts.txt"
+accounts={}
+
+#creating existing data
+def creating_account():
+    global accounts
+    file=open('Account_file.txt','w')
+    Account_file=file.readlines()
+    file.close()
+
+def create_account():
+    Account_number=int(input("Enter new account number:"))
+    while Account_number in accounts
+    Account_number=int(input("Enter account new account number:"))
+
+
